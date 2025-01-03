a4c4939fc32ba132787d56ca4a4983ba0f7975a4
Juan Alburgas <111477971+jalburgas@users.noreply.github.com>
2025-01-03 01:37:32 -0400

    inicial

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Aplicacion todolist</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="text" name="tarea" placeholder="Escribe tu tarea">
+        <input type="submit" value="Guardar">
+    </form>
+</body>
+</html>
